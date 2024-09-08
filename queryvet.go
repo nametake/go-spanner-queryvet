54810574af60b92e6596c914cb246dd3e9884c51
nametake <nametake.kyarabuki@gmail.com>
2024-09-08 12:55:09 +0900

    Added schema

--- queryvet.go
+++ queryvet.go
@@ -1,1 +1,18 @@
 package queryvet
+
+type DDL map[string]map[string]struct{}
+
+func (d DDL) Add(table, column string) {
+	if _, ok := d[table]; !ok {
+		d[table] = make(map[string]struct{})
+	}
+	d[table][column] = struct{}{}
+}
+
+func (d DDL) Has(table, column string) bool {
+	if _, ok := d[table]; !ok {
+		return false
+	}
+	_, ok := d[table][column]
+	return ok
+}
